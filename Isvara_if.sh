5b8fd774323110dbd0fc60f715e478f4ddc98ed9
Sheaffer-triumph <zoranlee0118@gmail.com>
2024-03-16 14:20:25 +0800

    did a little change in the file Isvara_if.sh

--- Isvara_if.sh
+++ Isvara_if.sh
@@ -30,6 +30,14 @@ if [ -f $a -a -r $a -o \( -d $a -a -x $a \) ]; then echo "1"; else echo "0"; fi 
 if [ -f $a -a \( -r $a -o -d $a \) -a \( -x $a -o -w $a \) ]; then echo "1"; else echo "0"; fi  #如果$a是文件并且（可读或者是文件夹）并且（可执行或者可写），则输出1，否则输出0
 if [ -f $a -a \( -r $a -o -d $a \) -a \( -x $a -o -w $a \) -a \( -z $a -o -n $a \) ]; then echo "1"; else echo "0"; fi  #如果$a是文件并且（可读或者是文件夹）并且（可执行或者可写）并且（为空或者不为空），则输出1，否则输出0
 if [ -f $a -a \( -r $a -o -d $a \) -a \( -x $a -o -w $a \) -a \( -z $a -o -n $a \) -a \( $a = $b -o $a != $b \) ]; then echo "1"; else echo "0"; fi  #如果$a是文件并且（可读或者是文件夹）并且（可执行或者可写）并且（为空或者不为空）并且（等于或者不等于$b），则输出1，否则输出0
-
-
-
+if (( $a == 1 )); then echo "1"; else echo "0"; fi       #如果$a等于1，则输出1，否则输出0
+if (( $a != 1 )); then echo "1"; else echo "0"; fi       #如果$a不等于1，则输出1，否则输出0
+if (( $a > 1 )); then echo "1"; else echo "0"; fi        #如果$a大于1，则输出1，否则输出0
+if (( $a < 1 )); then echo "1"; else echo "0"; fi        #如果$a小于1，则输出1，否则输出0
+if (( $a >= 1 )); then echo "1"; else echo "0"; fi       #如果$a大于等于1，则输出1，否则输出0
+if (( $a <= 1 )); then echo "1"; else echo "0"; fi       #如果$a小于等于1，则输出1，否则输出0
+if (( $a == 1 && $b == 1 )); then echo "1"; else echo "0"; fi  #如果$a等于1并且$b等于1，则输出1，否则输出0
+if (( $a == 1 || $b == 1 )); then echo "1"; else echo "0"; fi  #如果$a等于1或者$b等于1，则输出1，否则输出0
+if (( $a == 1 && ( $b == 1 || $c == 1 ) )); then echo "1"; else echo "0"; fi  #如果$a等于1并且（$b等于1或者$c等于1），则输出1，否则输出0
+if (( $a == 1 && ( $b == 1 || $c == 1 ) && ( $d == 1 || $e == 1 ) )); then echo "1"; else echo "0"; fi  #如果$a等于1并且（$b等于1或者$c等于1）并且（$d等于1或者$e等于1），则输出1，否则输出0
+if (( $a == 1 && ( $b == 1 || $c == 1 ) && ( $d == 1 || $e == 1 ) && ( $f == 1 || $g == 1 ) )); then echo "1"; else echo "0"; fi  #如果$a等于1并且（$b等于1或者$c等于1）并且（$d等于1或者$e等于1）并且（$f等于1或者$g等于1），则输出1，否则输出0
