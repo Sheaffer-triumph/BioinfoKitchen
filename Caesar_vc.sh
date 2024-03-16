b2341899e71abca98e75d07928d33ee7254bd713
lizhuoran <lizhuoran1@genomics.cn>
2024-03-16 17:54:13 +0800

    did a change in the file

--- Caesar_vc.sh
+++ Caesar_vc.sh
@@ -4,26 +4,37 @@
 
 set -e
 
-grep 
+grep "clustered" genome_by_genome_overview.csv | cut -d',' -f1 | grep id > cluster.id   ##此处没有通用代码，根据自己需要，在genome_by_genome_overview.csv中提取形成clustered的样本序列ID，输入至cluster.id
 
-
+rm cluster.txt
 cat cluster.id | while read a
     do
-    printf ${a}\\t >> cluster.txt
-    b=$(rg "\b$a\b" cluster.csv | cut -d',' -f10)
-    printf ${b}\\t >> cluster.txt
-    rg $b genome_by_genome_overview.csv | cut -d',' -f2-7 | grep -v "Unassigned,Unassigned,Unassigned,Unassigned,Unassigned,Unassigned" | sort | uniq > temp
-    c=$(cat temp | wc -l)
-    if (( $c == 0 ))
+    printf "${a}\t" >> cluster.txt
+    b=$(grep "\b$a\b" genome_by_genome_overview.csv | cut -d',' -f10)
+    printf "${b}\t" >> cluster.txt
+    c=$(grep $b genome_by_genome_overview.csv | cut -d',' -f2-7 | grep -v "Unassigned,Unassigned,Unassigned,Unassigned,Unassigned,Unassigned" | sort | uniq)
+    d=$(echo $c | wc -l)
+    if [ -z "$c" ]
     then
-        printf "N/A\n" >> cluster.txt
-    elif (( $c == 1))
+        printf "Unassigned,Unassigned,Unassigned,Unassigned,Unassigned,Unassigned\n" >> cluster.txt
+    elif (( $d == 1 ))
     then
-        d=$(cat temp)
-        printf ${d}\\n >> cluster.txt
+        printf "$c\n" >> cluster.txt
     else
-        e=$(cat temp | cut -f1-5 -d',' | sort | uniq)
-        printf ${e}\\n >> cluster.txt
+        e=0
+        g=1
+        until [ $g -gt 1 ]
+        do
+            e=$(expr $e + 1)
+            f=$(awk -F ',' -v var=$e '{print $var}' | sort | uniq)
+            g=$(echo $f | wc -l)
+        done
+        printf "$f" >> cluster.txt
+        g=$(expr 6 - $e)
+        for h in $(seq 1 $g)
+        do
+            printf ",Unassigned" >> cluster.txt
+        done
+        printf "\n" >> cluster.txt
     fi
 done
-rm temp
