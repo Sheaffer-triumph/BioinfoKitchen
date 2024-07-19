3faf9b05ae0428f5cbfe31a91d6c5e57f9b083dc
Sheaffer-triumph <zoranlee0118@gmail.com>
2024-07-19 21:47:01 +0800

    did a lot of changes

---
+++ Christos_autoqsub.sh
@@ -0,0 +1,128 @@
+#实现任务自动化投递的脚本，可以根据设定的参数，自动投递任务，无需手动操作
+#使用路径：/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/envs/amita/bin/autoqsub
+
+#更新时间：2024年7月11日
+#更新内容：增加了一个模块，用于在autoqsub运行时的即时修改参数，原理是使用kill向autoqsub所在的进程发送信号，autoqsub接收到信号后会重新读取参数文件，从而实现参数的修改
+
+#更新时间：2024年7月5日
+#更新内容：增加了一个选项-d，用于在每次循环中监测磁盘占用空间，当磁盘占用空间大于设定值时，会暂停正在运行的任务；设定值支持浮点数
+#备注：由于增加的模块只有暂停提交任务的功能，没有清理磁盘的功能，因此所提交的任务最好含有一些用于清理中间文件的命令，以免一直暂停
+#备注：监测当前磁盘空间的命令会根据不同的服务器而有所不同，需要根据实际情况进行修改
+#备注：这个功能并没有实装在集群中，因为在实际使用中，qhold并不能立即暂停任务，需要等待当前任务的正在执行的命令运行完后才会暂停
+
+#!/usr/bin/bash -e
+
+JOBNUM=20
+PROCESS=8
+MEM=50
+SLEEPTIME=5
+
+while getopts :l:n:p:m:s:d:hv opt
+do
+    case $opt in
+        l)
+            LIST=$OPTARG
+            ;;
+        n)
+            JOBNUM=$OPTARG
+            ;;
+        p)
+            PROCESS=$OPTARG
+            ;;
+        m)
+            MEM=$OPTARG
+            ;;
+        s)
+            SLEEPTIME=$OPTARG
+            ;;
+        d)
+            DISKLIMIT=$OPTARG
+            ;;
+        h)
+            cat /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.store/autoqsub_help.txt
+            exit
+            ;;
+        v)
+            echo "Autoqsub v1.1.0"
+            exit
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            cat /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.store/autoqsub_help.txt
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $LIST ] || [ -z $JOBNUM ] || [ -z $PROCESS ] || [ -z $MEM ] || [ -z $TIME ]
+    then
+    echo "ERROR! There are options with missing parameters, check the command"
+    cat /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.store/autoqsub_help.txt
+    exit 1
+fi
+
+cat << EOF > .parameter2autoqsub
+Jobnum ${JOBNUM}
+Memory ${MEM}
+Process ${PROCESS}
+EOF
+
+handle_usr1() 
+{
+    JOBNUM=$(grep "Jobnum" | awk '{print $2}')
+    MEM=$(grep "Memory" | awk '{print $2}')
+    PROCESS=$(grep "Process" | awk '{print $2}')
+    echo $(date) >> autoqsub.log
+    echo "Autoqsub trap the signal. Jobnum, Memory and Process have been changed" >> autoqsub.log
+}
+
+trap 'handle_usr1' USR1
+
+WDIR=$(pwd -P)
+cp $LIST .tobeqsub.list
+a=$(cat .tobeqsub.list | wc -l)                         #a表示还需要提交的任务数
+until [ $a == 0 ]                                       #until表示直到a等于0才停止
+do
+    b=$(qstat | grep "lizhuoran1" | wc -l)                                   
+    if [ $b -lt $JOBNUM ]                               #如果当前正在运行的任务数小于设定值，则继续提交任务
+    then    
+        d=$(expr $JOBNUM - $b)
+        head -n $d .tobeqsub.list > .qsub.list
+        grep -wvf .qsub.list .tobeqsub.list > .tmp.list || true      #在tobeqsub.list中去除qsub.list中的内容，-w表示精确匹配，-v表示取反，-f表示从文件中读取内容
+        cat .tmp.list > .tobeqsub.list
+        f=`date`
+        echo $f >> autoqsub.log
+        for e in $(cat .qsub.list)
+        do
+            cd $(dirname $e)
+            qsub -cwd -q st.q -P P17Z10200N0246 -l vf=${MEM}g,num_proc=$PROCESS -binding linear:$PROCESS $e
+            cd $WDIR
+            echo "$e has been qsub" >> autoqsub.log
+        done
+    else
+        f=`date`
+        echo $f >> autoqsub.log
+        echo "There are $JOBNUM jobs running, please wait" >> autoqsub.log
+    fi
+    if [ -n $DISKLIMIT]
+    then
+        DiskQuota=$(lfs quota -gh st_p17z10200n0246 /ldfssz1/ | sed '1,2d' | head -n 1 | awk '{print $2}' | sed 's/T//g')
+        THAN=$(echo "$DiskQuota > $DISKLIMIT" | bc)
+        if [ $THAN -eq 1 ]
+        then
+            echo "Disk space has exceeded the set limit and the sh has been suspended" >> autoqsub.log
+            qstat | sed '1,2d' | grep -w r | awk '{print $1}' | while read id
+            do
+                qhold $id
+            done
+        else
+            qstat | sed '1,2d' | grep -w hr | awk '{print $1}' | while read id
+            do
+                qrls $id
+            done
+        fi
+    sleep ${SLEEPTIME}m                                        #sleep 2m表示休眠2分钟
+
+done
+echo "All jobs have been qsub" >> autoqsub.log
+/usr/bin/rm -rf .tobeqsub.list .qsub.list .tmp.list
---
+++ Christos_bashrc.sh
@@ -0,0 +1,120 @@
+# .bashrc
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+#Predefined environment
+export PS1="\[\e[33;1m\]\u@\h \[\e[35;1m\]\d \t \[\e[36;1m\]\w\[\e[0m\]\n\[\e[32;1m\]$ \[\e[0m\]"
+export TERMINFO=/usr/share/terminfo
+export PATH="/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/meme/bin:$PATH"
+export PATH="/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/envs/hamburger/bin/:$PATH"
+export PATH="/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/envs/amita/bin/:$PATH"
+
+#Predefined shortcut commands
+alias ll='ls -lthr'
+alias compute='ssh cngb-xcompute-0-10'
+alias software='ssh cngb-software-0-1'
+alias lzr1='cd /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1'
+alias lzr2='cd /hwfssz5/ST_HEALTH/P17Z10200N0246/USER/lizhuoran1'
+alias bashinit='source ~/.bashrc'
+alias where='realpath'
+alias pwd='pwd -P'
+alias jobs='jobs -l'
+alias htop-u='/usr/bin/htop -u lizhuoran1'
+alias htop='/usr/bin/htop'
+alias mambainit='source ~/.mamba_init.sh'
+alias activate='mamba activate'
+alias deactivate='mamba deactivate'
+alias rm=trash
+alias rm!='/usr/bin/rm -rf'
+alias clt=cleantrash
+alias rc=trashrecycle
+alias rl=removelist
+alias rdu='ncdu /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.Trash'
+
+#Predefined Functions
+#Simulate the Recycle Bin in Linux
+function disk()
+{
+	lfs quota -gh st_p17z10200n0246 /ldfssz1/ | sed '1d' | head -n 2 | sed 's/^ *//'
+	df -h  /hwfssz5/ST_HEALTH/P17Z10200N0246/USER/lizhuoran1
+}
+function trash()
+{
+	for file in "$@"; do
+	path=$(realpath $file)
+	i=$(echo $path | sed 's/\//~/g' | sed 's/~ldfssz1~ST_HEALTH~P17Z10200N0246~lizhuoran1//g')
+	date=$(echo $(date +%F-%R))
+	mv -f $file /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.Trash/${date}$i
+	done
+}
+function cleantrash()
+{
+	read -p "Confirm if you want to empty the Trash (y or n):" confirm
+	[ $confirm == 'y' ] && /usr/bin/rm -rf /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.Trash/*
+}
+function trashrecycle()
+{
+	for file in "$@"; do
+	mv -i /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.Trash/$@ ./
+	i=$(echo $file | awk -F '~' '{print $NF}')
+	mv $file $i
+	done
+}
+function removelist()
+{
+	ll /ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/.Trash
+}
+#Retrieve historical commands
+function his()
+{
+	history | grep $1
+}
+#easier qstat
+function qg()
+{
+	qstat | grep $1
+}
+function qcd()
+{
+	OPTIND=1
+	while getopts :n:i: opt
+	do
+        case "$opt" in
+            n)
+                cd $(qstat -j $(qstat | sed '1,2d' | awk '{print $1}' | sed -n "${OPTARG}p") | grep cwd | awk '{print $2}')
+			;;
+            i)
+                cd $(qstat -j ${OPTARG} | grep cwd | awk '{print $2}')
+			;;
+        esac
+    done
+}
+#easier qsub
+function qs()
+{
+	qsub -cwd -q st.q -P P17Z10200N0246 -l vf=${1}g,num_proc=$2 -binding linear:$3 $4
+}
+function qs_super()
+{
+	qsub -cwd -l vf=${1}g,num_proc=$2 -P P17Z10200N0246_super -binding linear:$3 -q st_supermem.q $4
+}
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+#__conda_setup="$('/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#    eval "$__conda_setup"
+#else
+#    if [ -f "/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/etc/profile.d/conda.sh" ]; then
+#        . "/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
+
+#if [ -f "/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/etc/profile.d/mamba.sh" ]; then
+#    . "/ldfssz1/ST_HEALTH/P17Z10200N0246/lizhuoran1/software/miniconda/etc/profile.d/mamba.sh"
+#fi
+# <<< conda initialize <<<
