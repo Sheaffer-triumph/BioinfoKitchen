c00ab8ec65048e4fc7fa3c68bd89c0063768cc12
Sheaffer-triumph <zoranlee0118@gmail.com>
2024-10-22 20:25:46 +0800

    diid

---
+++ Ananda_cloud.sh
@@ -0,0 +1,7 @@
+#云平台使用
+#dvf镜像配置
+mamba install -y -c conda-forge -c bioconda seqkit python=3.6 numpy theano=1.0.3 keras=2.2.4 scikit-learn Biopython h5py=2.10.0
+
+#iphop_with_db
+db_path = "/data/input/Files/ReferenceData/iphop_database/Aug_2023_pub_rw"
+
